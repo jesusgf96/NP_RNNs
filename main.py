48e4e59f5e29a37dc7c9ade51447b250c711fb90
Jesús <vitolapata@gmail.com>
2024-06-10 12:13:33 +0200

    Final version

--- main.py
+++ main.py
@@ -4,29 +4,34 @@ from utils import *
 
 
 # Choose GPU
-device = torch.device("cuda:1" if torch.cuda.is_available() else "cpu")
+device = torch.device("cuda:4" if torch.cuda.is_available() else "cpu")
 
 # Parameters
-seed = 42 #381 #42
-dataset = 'mackey-glass' #'psMNIST' #'copy'
-algorithm = 'ANP' #'NP' #'WP' #'BP'
-batch_size = 10 #1 #100
-lr_fwd = 5e-5
-lr_decor = 5e-8
-decorrelation = False
+# seed =  42  101 300  482  708 
+seed = 42
+dataset = 'copy' #'mackey-glass' #'weather' #'copy' #'psMNIST' 
+algorithm = 'WP' #'NP' #'ANP' #'BP'
+batch_size = 1 #10 #100
+lr_fwd = 0.001
+lr_decor = 1e-4 #1e-7
+decorrelation = True
+input_decorrelation = False
 hidden_units = 500 #1000
 epochs = 500
-noise_std = 0.001
+noise_std = 0.001 
 act_func = tanh()
 act_func_out = linear()
 loss_name = 'mse' #'ce'
 opt = 'adam' #'sgd'
+
 
 # Define network architecture
 if dataset == 'copy':
     net_structure = [10, hidden_units, 10]
 elif dataset == 'psMNIST':
     net_structure = [1, hidden_units, 10]
+elif dataset == 'weather':
+    net_structure = [9, hidden_units, 1]
 else:
     net_structure = [1, hidden_units, 1]
 
@@ -34,15 +39,19 @@ else:
 # Training loop
 if dataset == 'mackey-glass':
     performances, correlation, model = training_mackey_glass(net_structure=net_structure, act_func=act_func, act_func_out=act_func_out, epochs=epochs, 
-                                                             algorithm=algorithm, batch_size=batch_size, lr_fwd=lr_fwd, decorrelation=decorrelation, 
+                                                             algorithm=algorithm, batch_size=batch_size, lr_fwd=lr_fwd, decorrelation=decorrelation, input_decorrelation=input_decorrelation,
                                                              lr_decor=lr_decor, noise_std=noise_std, loss_name=loss_name, opt=opt, device=device, seed=seed)
 elif dataset == 'copy':
     performances, correlation, model = training_copy(net_structure=net_structure, act_func=act_func, act_func_out=act_func_out, epochs=epochs, 
-                                                     algorithm=algorithm, batch_size=batch_size, lr_fwd=lr_fwd, decorrelation=decorrelation, 
+                                                     algorithm=algorithm, batch_size=batch_size, lr_fwd=lr_fwd, decorrelation=decorrelation, input_decorrelation=input_decorrelation,
                                                      lr_decor=lr_decor, noise_std=noise_std, loss_name=loss_name, opt=opt, device=device, seed=seed)
 elif dataset == 'psMNIST':
     performances, correlation, model = training_psMNIST(net_structure=net_structure, act_func=act_func, act_func_out=act_func_out, epochs=epochs, 
-                                                        algorithm=algorithm, batch_size=batch_size, lr_fwd=lr_fwd, decorrelation=decorrelation, 
+                                                        algorithm=algorithm, batch_size=batch_size, lr_fwd=lr_fwd, decorrelation=decorrelation, input_decorrelation=input_decorrelation,
+                                                        lr_decor=lr_decor, noise_std=noise_std, loss_name=loss_name, opt=opt, device=device, seed=seed)
+elif dataset == 'weather':
+    performances, correlation, model = training_weather(net_structure=net_structure, act_func=act_func, act_func_out=act_func_out, epochs=epochs, 
+                                                        algorithm=algorithm, batch_size=batch_size, lr_fwd=lr_fwd, decorrelation=decorrelation, input_decorrelation=input_decorrelation,
                                                         lr_decor=lr_decor, noise_std=noise_std, loss_name=loss_name, opt=opt, device=device, seed=seed)
 else:
     print('Dataset not supported!')
